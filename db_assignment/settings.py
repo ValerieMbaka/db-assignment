e1c50c99876469b0cf91003fcdd36580262a57ae
ValerieMbaka <valeriembaka0@gmail.com>
2024-11-16 17:15:13 +0300

    db

--- db_assignment/settings.py
+++ db_assignment/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'db_assignment.wsgi.application'
 DATABASES = {
         'default': {
                 'ENGINE' : 'django.db.backends.mysql',
-                'NAME' : 'arsha',
+                'NAME' : 'dbassignment',
                 'USER' : 'root',
                 'PASSWORD' : '',
                 'HOST' : '127.0.0.1',
